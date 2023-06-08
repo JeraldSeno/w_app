cc09b3e46d2f2b2b79b613b7b206ccb30a87dc65
Jerald Seno <jeraldseno.js@gmail.com>
2023-06-09 07:19:12 +0800

    Update api.js

--- Weather_App/src/api.js
+++ Weather_App/src/api.js
@@ -1,11 +1,11 @@
 export const geoApiOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "ae78ac3672msh4ded80343f41a25p153b9bjsnb02df7e16369",// enter your rapid api key here
+    "X-RapidAPI-Key": "",// enter your rapid api key here
     "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
   },
 };
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-export const WEATHER_API_KEY = "38bc1b4d522f76e669e3144e6e1b96a3";
+export const WEATHER_API_KEY = "";
